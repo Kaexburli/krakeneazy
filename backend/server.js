d629aa540ec7ff35d7077f90675dee1fc1fd11a6
Franck Pichard <pichard.f67@gmail.com>
2021-12-26 19:08:06 +0100

    Fichier server Fastify

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+// Require the framework and instantiate it
+import Fastify from 'fastify'
+import FastifySwagger from 'fastify-swagger'
+import FastifyEnv from 'fastify-env'
+import FastifyCors from 'fastify-cors'
+import FastifyWs from 'fastify-websocket'
+import Autoload from 'fastify-autoload'
+
+const fastify = Fastify({ logger: { level: 'trace' } })
+const PORT = process.env.PORT || '9000'
+
+
+const options = {
+  schema: {
+    type: 'object',
+    properties: {
+      API_KEY: { type: 'string' },
+      API_PRIV: { type: 'string' }
+    }
+  },
+  dotenv: true
+}
+
+fastify.register(FastifySwagger, {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: { title: 'fastify-api' },
+  },
+})
+
+fastify.register(FastifyEnv, options)
+fastify.register(FastifyCors)
+fastify.register(FastifyWs)
+
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+fastify.register(Autoload, {
+  dir: './api/routes/'
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
