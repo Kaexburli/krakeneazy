f0f22ac2c1f771ab06e3a530d62631923a324988
Franck Pichard <pichard.f67@gmail.com>
2021-12-26 19:25:39 +0100

    [Frontend] Ajout du dossier components/pages/reports

---
+++ frontend/src/components/pages/reports/Reports.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { fade, scale } from "svelte/transition";
+</script>
+
+<div
+  id="page-reports"
+  in:scale={{ delay: 200, duration: 300 }}
+  out:fade={{ delay: 1, duration: 1 }}
+>
+  <h1>Reports</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
+    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
+    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
+  </p>
+</div>
