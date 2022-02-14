37af1a4ec0511d9191d193765c8a3170b448f834
Kaexburli <pichard.f67@gmail.com>
2022-02-14 23:17:47 +0100

    [FRONTEND] (en cours) Page settings + traduction (Menu + contenu formulaire)

--- frontend/src/components/pages/settings/MainSettings.svelte
+++ frontend/src/components/pages/settings/MainSettings.svelte
@@ -1,67 +1,38 @@
 <script>
+  import { _ } from "svelte-i18n";
   import { slide } from "svelte/transition";
+
+  import DrawerList from "./DrawerList.svelte";
+  import Profile from "./profile.svelte";
+  import Tab2 from "./kraken.svelte";
+  import Tab3 from "./settings.svelte";
+
+  let items = [
+    {
+      label: $_("settings.drawer.labels.profile"),
+      subtitle: $_("settings.drawer.subtitle.profile"),
+      position: 1,
+      component: Profile,
+    },
+    {
+      label: $_("settings.drawer.labels.kraken"),
+      subtitle: $_("settings.drawer.subtitle.kraken"),
+      position: 2,
+      component: Tab2,
+    },
+    {
+      label: $_("settings.drawer.labels.settings"),
+      subtitle: $_("settings.drawer.subtitle.settings"),
+      position: 3,
+      component: Tab3,
+    },
+  ];
 </script>
 
 <div id="page-settings" in:slide out:slide>
-  <h1>Settings</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti iste
-    voluptatibus quos? Sed assumenda quaerat vitae consectetur quis culpa magnam
-    nihil reiciendis laborum eligendi cum nam dolorum, autem deserunt ut.
-  </p>
+  <h1>{$_("settings.title")}</h1>
+
+  <div class="box">
+    <DrawerList {items} />
+  </div>
 </div>
---
+++ frontend/src/components/pages/settings/profile.svelte
@@ -0,0 +1,123 @@
+<script>
+  import { _ } from "svelte-i18n";
+  import { User } from "store/userStore.js";
+
+  let profile = false;
+  $: if ($User) {
+    (async () => {
+      profile = await User.getProfile();
+    })();
+  }
+
+  import FormField from "@smui/form-field";
+  import Radio from "@smui/radio";
+  import Button from "@smui/button";
+  import Checkbox from "@smui/checkbox";
+  import Textfield from "@smui/textfield";
+  import Icon from "@smui/textfield/icon";
+  import HelperText from "@smui/textfield/helper-text";
+  import LayoutGrid, { Cell } from "@smui/layout-grid";
+
+  let firstname = "",
+    lastname = "",
+    username = "",
+    email = "";
+</script>
+
+{#if profile}
+  <LayoutGrid>
+    <Cell span={6}>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.firstname.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={firstname}
+          label={profile.user.firstname}
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.firstname.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.lastname.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={lastname}
+          label={profile.user.lastname}
+          autocomplete="off"
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.lastname.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.password.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={email}
+          label=""
+          autocomplete="off"
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.password.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+    </Cell>
+    <Cell span={6}>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.username.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={username}
+          label={profile.user.username}
+          autocomplete="off"
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.username.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.email.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={email}
+          label={profile.user.email}
+          autocomplete="off"
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.email.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+      <div class="margins mbot">
+        <h4>{$_("settings.profile.passwordConfirm.label")}</h4>
+        <Textfield
+          style="width: 100%;"
+          helperLine$style="width: 100%;"
+          bind:value={email}
+          label=""
+          autocomplete="off"
+        >
+          <HelperText slot="helper"
+            >{$_("settings.profile.passwordConfirm.helper")}</HelperText
+          >
+        </Textfield>
+      </div>
+    </Cell>
+  </LayoutGrid>
+{/if}
+
+<style>
+  .mbot {
+    margin-bottom: 10px;
+  }
+</style>
