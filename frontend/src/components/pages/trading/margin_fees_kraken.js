c80696200c4f230769d0be6660849df116cd4918
Kaexburli <pichard.f67@gmail.com>
2022-04-08 17:29:09 +0200

    [FROTNEND] Ajout de la liste des frais de marge

---
+++ frontend/src/components/pages/trading/margin_fees_kraken.js
@@ -0,0 +1,238 @@
+export const assetsFees = {
+  "ALGO": {
+    "name": "Algorand",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "XBT": {
+    "name": "Bitcoin",
+    "quote": [
+      "EUR",
+      "USD",
+      "USDT"
+    ],
+    "percent": "0.01",
+    "rollover": "0.01"
+  },
+  "USDT": {
+    "name": "Tether",
+    "quote": [
+      "EUR",
+      "USD"
+    ],
+    "percent": "0.01",
+    "rollover": "0.01"
+  },
+  "USDC": {
+    "name": "USD Coin",
+    "quote": [
+      "EUR",
+      "USD"
+    ],
+    "percent": "0.01",
+    "rollover": "0.01"
+  },
+  "REP": {
+    "name": "Augur",
+    "quote": [
+      "EUR",
+      "ETH",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "BCH": {
+    "name": "Bitcoin Cash",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "ADA": {
+    "name": "Cardano",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "Link": {
+    "name": "Chainlink",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "ATOM": {
+    "name": "Cosmos",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "DASH": {
+    "name": "Dash",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "EOS": {
+    "name": "EOS",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "ETH": {
+    "name": "Ethereum",
+    "quote": [
+      "EUR",
+      "USD",
+      "USDT",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "ETC": {
+    "name": "Ethereum Classic",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "FIL": {
+    "name": "Filecoin",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "LTC": {
+    "name": "Litecoin",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "XMR": {
+    "name": "Monero",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "OMG": {
+    "name": "OmiseGO",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "DOT": {
+    "name": "Polkadot",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "XRP": {
+    "name": "Ripple",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "XLM": {
+    "name": "Stellar Lumens",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "XTZ": {
+    "name": "Tezos",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "TRX": {
+    "name": "Tron",
+    "quote": [
+      "ETH",
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  },
+  "ZEC": {
+    "name": "Zcash",
+    "quote": [
+      "EUR",
+      "USD",
+      "XBT"
+    ],
+    "percent": "0.02",
+    "rollover": "0.02"
+  }
+}
