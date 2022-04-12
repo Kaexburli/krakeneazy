092bd95b4047182966a0768b8de002e8fb89be1a
Kaexburli <pichard.f67@gmail.com>
2022-04-12 14:14:29 +0200

    [BACKEND] Déplacement des fichier html des erreurs pour utilisation global

--- backend/server.js
+++ backend/server.js
@@ -69,15 +69,15 @@ else {
   fastify.setErrorHandler((error, request, reply) => {
     var statusCode = reply.statusCode
     if (statusCode >= 500) {
-      const pageNotFoundStream = fs.createReadStream('./public/500.html')
+      const pageNotFoundStream = fs.createReadStream('../error_pages/500.html')
       fastify.log.error(`[SERVER setErrorHandler ${statusCode}]:${error}`)
       reply.code(500).type('text/html').send(pageNotFoundStream)
     } else if (statusCode >= 400) {
-      const pageNotFoundStream = fs.createReadStream('./public/400.html')
+      const pageNotFoundStream = fs.createReadStream('../error_pages/400.html')
       fastify.log.error(`[SERVER setErrorHandler ${statusCode}]:${error}`)
       reply.code(400).type('text/html').send(pageNotFoundStream)
     } else {
-      const pageNotFoundStream = fs.createReadStream('./public/maintenance.html')
+      const pageNotFoundStream = fs.createReadStream('../error_pages/maintenance.html')
       fastify.log.error(`[SERVER setErrorHandler]:${error}`)
       reply.code(404).type('text/html').send(pageNotFoundStream)
     }
