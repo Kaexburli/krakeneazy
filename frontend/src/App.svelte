e1a7c735c063bd620e43451eb402d9967226c2eb
Kaexburli <pichard.f67@gmail.com>
2022-04-19 01:22:02 +0200

    [FRONTEND] Ajout du Tag Google Analytics

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,25 @@
 <script>
   import Layout from "components/layout/Layout.svelte";
+
+  const env = __App["env"].ENVIRONMENT;
 </script>
+
+<svelte:head>
+  {#if env === "production"}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-9Z0L8Q1CNS"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-9Z0L8Q1CNS");
+    </script>
+  {/if}
+</svelte:head>
 
 <Layout />
